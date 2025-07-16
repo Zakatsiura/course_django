ef6a73275b4d4e3634223460227095925ba6448a
Zakatsiura <zakatsiura@gmail.com>
2025-07-16 19:02:12 +0300

    #Final

--- general/views.py
+++ general/views.py
@@ -1,11 +1,11 @@
 from django.views.generic import TemplateView
+from hr.models import Company 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        from hr.models import Company 
         company = Company.objects.first()
         context['company_logo'] = company.logo.url if company and company.logo else None
         return context
