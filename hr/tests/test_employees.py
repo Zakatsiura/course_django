72bf652abddbabada91a6a1c62e736dd6955dce3
Zakatsiura <zakatsiura@gmail.com>
2025-08-04 21:26:58 +0300

    #Task 1

--- hr/tests/test_employees.py
+++ hr/tests/test_employees.py
@@ -3,6 +3,7 @@ from django.test import (
     Client,
     TestCase,
 )
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 
 from hr.models import Employee
@@ -93,3 +94,91 @@ class EmployeeCreateViewTest(TestCase):
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Працівника успішно створено.')
 
+
+class EmployeeProfileViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = EmployeeFactory(is_staff=True, is_superuser=True)
+
+        avatar_file = SimpleUploadedFile(
+            name='test_avatar.jpg',
+            content=b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00',
+            content_type='image/jpeg'
+        )
+        
+        cv_file = SimpleUploadedFile(
+            name='test_cv.pdf',
+            content=b'%PDF-1.4 test pdf content',
+            content_type='application/pdf'
+        )
+        self.employee = EmployeeFactory(avatar=avatar_file, cv=cv_file)
+
+        self.client.force_login(self.admin_user)
+        self.url = reverse('hr:employee_profile', kwargs={'pk': self.employee.pk})
+
+
+    def test_access_employee_profile(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'employee_profile.html')
+        self.assertEqual(response.context['employee'], self.employee)
+
+
+
+class EmployeeDeleteViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = EmployeeFactory(is_staff=True, is_superuser=True)
+        self.non_admin_user = EmployeeFactory(is_staff=False, is_superuser=False)
+        self.employee_to_delete = EmployeeFactory()
+        self.url = reverse('hr:employee_delete', kwargs={'pk': self.employee_to_delete.pk})
+
+    def test_delete_employee_by_admin(self):
+        self.client.force_login(self.admin_user)
+        response = self.client.post(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Employee.objects.filter(pk=self.employee_to_delete.pk).exists())
+
+    def test_delete_employee_by_non_admin(self):
+        self.client.force_login(self.non_admin_user)
+        response = self.client.post(self.url)
+        self.assertNotEqual(response.status_code, 302)
+        self.assertTrue(Employee.objects.filter(pk=self.employee_to_delete.pk).exists())
+
+
+class EmployeeUpdateViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = EmployeeFactory(is_staff=True, is_superuser=True)
+        self.non_admin_user = EmployeeFactory(is_staff=False, is_superuser=False)
+        self.employee_to_update = EmployeeFactory(first_name='OldName')
+        self.position = PositionFactory()
+        self.url = reverse('hr:employee_update', kwargs={'pk': self.employee_to_update.pk})
+
+    def test_update_employee_by_admin(self):
+        self.client.force_login(self.admin_user)
+        updated_data = {
+            'username': self.employee_to_update.username,
+            'first_name': 'NewName',
+            'last_name': self.employee_to_update.last_name,
+            'email': self.employee_to_update.email,
+            'position': self.position.id,
+        }
+        response = self.client.post(self.url, updated_data)
+        self.assertEqual(response.status_code, 302)
+        self.employee_to_update.refresh_from_db()
+        self.assertEqual(self.employee_to_update.first_name, 'NewName')
+
+    def test_update_employee_by_non_admin(self):
+        self.client.force_login(self.non_admin_user)
+        updated_data = {
+            'username': self.employee_to_update.username,
+            'first_name': 'NewName',
+            'last_name': self.employee_to_update.last_name,
+            'email': self.employee_to_update.email,
+            'position': self.position.id,
+        }
+        response = self.client.post(self.url, updated_data)
+        self.assertNotEqual(response.status_code, 302)
+        self.employee_to_update.refresh_from_db()
+        self.assertNotEqual(self.employee_to_update.first_name, 'NewName')
